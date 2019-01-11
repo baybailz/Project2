addcc39e480e7e3be6ce242f324abd6276a99600
Megan Gray <megjgray@gmail.com>
2019-01-11 16:33:26 -0500

    Bailey's update

--- app.py
+++ app.py
@@ -50,45 +50,45 @@ def years():
     print(year)
     return jsonify(year)
 
-# @app.route("/fire_metadata/<year>")
-# def fire_metadata(year):
+@app.route("/fire_metadata/<year>")
+def fire_metadata(year):
     
-#     sel = [
-#     Fire.Fire_name,
-#     Fire.Fire_year,
-#     Fire.Fire_Size,
-#     Fire.Discovery_DOY]
+    sel = [
+    Fire.Fire_name,
+    Fire.Fire_year,
+    Fire.Fire_Size,
+    Fire.Discovery_DOY]
     
-#     results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
+    results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
     
-#     df_year = pd.DataFrame(results)
+    df_year = pd.DataFrame(results)
     
-#     fire_metadata = {}
-#     fire_metadata["Area Burned"] = round(sum(df_year['Fire_Size']),2)
-#     fire_metadata["Number of Fires"] = len(df_year)
+    fire_metadata = {}
+    fire_metadata["Area Burned"] = round(sum(df_year['Fire_Size']),2)
+    fire_metadata["Number of Fires"] = len(df_year)
 
-#     return jsonify(fire_metadata)
+    return jsonify(fire_metadata)
 
-# @app.route("/years/<year>")
-# def year(year):
-#     """Return fire name, fire_DOY, and acres damaged."""
-#     sel = [
-#     Fire.Fire_name,
-#     Fire.Fire_year,
-#     Fire.Fire_Size,
-#     Fire.Discovery_DOY]
+@app.route("/years/<year>")
+def year(year):
+    """Return fire name, fire_DOY, and acres damaged."""
+    sel = [
+    Fire.Fire_name,
+    Fire.Fire_year,
+    Fire.Fire_Size,
+    Fire.Discovery_DOY]
     
-#     results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
-#     df_yearly = pd.DataFrame(results)
+    results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
+    df_yearly = pd.DataFrame(results)
 
 
-#     fire_data = {}
-#     # Format the data to send as json
-#     fire_data["Fire_Name"] = df_yearly['Fire_name']
-#     fire_data["Fire_size"] = df_yearly['Fire_Size']
-#     fire_data["Discovery_DOY"] = df_yearly['Discovery_DOY']
+    fire_data = {}
+    # Format the data to send as json
+    fire_data["Fire_Name"] = df_yearly['Fire_name']
+    fire_data["Fire_size"] = df_yearly['Fire_Size']
+    fire_data["Discovery_DOY"] = df_yearly['Discovery_DOY']
 
-#     return jsonify(fire_data)
+    return jsonify(fire_data)
 
 
 if __name__ == "__main__": 
