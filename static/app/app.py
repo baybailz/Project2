66dd67963d5c0b0b0741ce1f0f99d24c5f1d663d
baybailz <bthomp73@uncc.edu>
2019-01-10 19:46:31 -0500

    new app.py

--- static/app/app.py
+++ static/app/app.py
@@ -26,66 +26,70 @@ Base.prepare(db.engine, reflect=True)
 session = Session(engine)
 
 Fire = Base.classes.fire
+years = Base.classes.years
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
+
 @app.route("/years")
 def years():
     """Return a list years."""
 
     # Use Pandas to perform the sql query
-    sel_year = [
-    Fire.Fire_year]
-    results_year = db.session.query(*sel_year).all()
-    df_distinct_year = pd.DataFrame(results_year)
-    years = df_distinct_year["Fire_year"].unique()
-    print(years)
-
-    return jsonify(years)
-
-@app.route("/metadata/<year>")
-def fire_metadata(year):
+    results = db.session.query(Years.years).\
+        order_by(Years.years.desc())
     
-    sel = [
-    Fire.Fire_name,
-    Fire.Fire_year,
-    Fire.Fire_Size,
-    Fire.Discovery_DOY]
+    year = []
+    for result in results:
+        year.append({
+            "year": result[0]
+        })
+    print(year)
+    return jsonify(year)
+
+# @app.route("/fire_metadata/<year>")
+# def fire_metadata(year):
     
-    results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
+#     sel = [
+#     Fire.Fire_name,
+#     Fire.Fire_year,
+#     Fire.Fire_Size,
+#     Fire.Discovery_DOY]
     
-    df_year = pd.DataFrame(results)
+#     results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
     
-    fire_metadata = {}
-    fire_metadata["Area Burned"] = round(sum(df_year['Fire_Size']),2)
-    fire_metadata["Number of Fires"] = len(df_year)
-
-    return jsonify(fire_metadata)
-
-@app.route("/years/<year>")
-def year(year):
-    """Return fire name, fire_DOY, and acres damaged."""
-    sel = [
-    Fire.Fire_name,
-    Fire.Fire_year,
-    Fire.Fire_Size,
-    Fire.Discovery_DOY]
+#     df_year = pd.DataFrame(results)
     
-    results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
-    df_yearly = pd.DataFrame(results)
+#     fire_metadata = {}
+#     fire_metadata["Area Burned"] = round(sum(df_year['Fire_Size']),2)
+#     fire_metadata["Number of Fires"] = len(df_year)
+
+#     return jsonify(fire_metadata)
+
+# @app.route("/years/<year>")
+# def year(year):
+#     """Return fire name, fire_DOY, and acres damaged."""
+#     sel = [
+#     Fire.Fire_name,
+#     Fire.Fire_year,
+#     Fire.Fire_Size,
+#     Fire.Discovery_DOY]
+    
+#     results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
+#     df_yearly = pd.DataFrame(results)
 
 
-    fire_data = {}
-    # Format the data to send as json
-    fire_data["Fire Name"] = df_yearly['Fire_name']
-    fire_data["Fire_size"] = df_yearly['Fire_Size']
-    fire_data["Discovery_DOY"] = df_yearly['Discovery_DOY']
+#     fire_data = {}
+#     # Format the data to send as json
+#     fire_data["Fire_Name"] = df_yearly['Fire_name']
+#     fire_data["Fire_size"] = df_yearly['Fire_Size']
+#     fire_data["Discovery_DOY"] = df_yearly['Discovery_DOY']
 
-    return jsonify(fire_data)
+#     return jsonify(fire_data)
 
 
-if __name__ == "__main__": 
-    app.run()
+# if __name__ == "__main__": 
+#     app.run()
