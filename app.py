24f5a6ebfd07898f4bc08e1745c802bdc38c756b
Megan Gray <megjgray@gmail.com>
2019-01-11 20:11:29 -0500

    Webpage and Code Update

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///Fires.sqlite', echo=True)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Fires.sqlite"
+engine = create_engine('sqlite:///Fires.sqlite')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fires.sqlite"
 db = SQLAlchemy(app)
 
 Base = automap_base()
@@ -26,31 +26,25 @@ Base.prepare(db.engine, reflect=True)
 session = Session(engine)
 
 Fire = Base.classes.fire
-years = Base.classes.test
+
+Years = Base.classes.years
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
-
 @app.route("/years")
 def years():
     """Return a list years."""
 
     # Use Pandas to perform the sql query
-    results = db.session.query(Years.years).\
-        order_by(Years.years.desc())
+    stmt = db.session.query(Years).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
     
-    year = []
-    for result in results:
-        year.append({
-            "year": result[0]
-        })
-    print(year)
-    return jsonify(year)
+    return jsonify(list(df['years']))
 
-@app.route("/fire_metadata/<year>")
+@app.route("/metadata/<year>")
 def fire_metadata(year):
     
     sel = [
@@ -59,14 +53,19 @@ def fire_metadata(year):
     Fire.Fire_Size,
     Fire.Discovery_DOY]
     
-    results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
+    results1 = db.session.query(*sel).filter(Fire.Fire_year == year).all()
     
-    df_year = pd.DataFrame(results)
+    df_year = pd.DataFrame(results1)
     
-    fire_metadata = {}
-    fire_metadata["Area Burned"] = round(sum(df_year['Fire_Size']),2)
-    fire_metadata["Number of Fires"] = len(df_year)
-
+    fire_metadata = []
+    
+    fire_metadata.append({
+        "Acres_Burned": round(sum(df_year["Fire_Size"]),2),
+        "Cumulative_Days_of_Burning": sum(df_year["Discovery_DOY"]),
+        "Number_of_Fires": len(df_year)
+    }) 
+        
+    
     return jsonify(fire_metadata)
 
 @app.route("/years/<year>")
@@ -76,20 +75,55 @@ def year(year):
     Fire.Fire_name,
     Fire.Fire_year,
     Fire.Fire_Size,
-    Fire.Discovery_DOY]
+    Fire.Discovery_DOY,
+    Fire.Cause_Descr]
     
     results = db.session.query(*sel).filter(Fire.Fire_year == year).all()
     df_yearly = pd.DataFrame(results)
 
+    df_yearly = df_yearly.dropna()
 
-    fire_data = {}
-    # Format the data to send as json
-    fire_data["Fire_Name"] = df_yearly['Fire_name']
-    fire_data["Fire_size"] = df_yearly['Fire_Size']
-    fire_data["Discovery_DOY"] = df_yearly['Discovery_DOY']
+    df_grp_fs = df_yearly.groupby("Cause_Descr").mean()["Fire_Size"]
+    df_grp_fs = df_grp_fs.reset_index()
 
-    return jsonify(fire_data)
+    # print(df_yearly)
+
+    # fire_data = []
+    # # Format the data to send as json
+    # fire_data.append({
+        
+    # }) = df_yearly['Fire_name']
+    # fire_data["Fire_size"] = df_yearly['Fire_Size']
+    # fire_data["Discovery_DOY"] = df_yearly['Discovery_DOY']
+
+    df_yearly_dict = df_grp_fs.to_dict(orient="record")
+
+    return jsonify(df_yearly_dict)
+
+@app.route("/bigfires/<year>")
+def bigfires(year):
+
+    big = [
+        Fire.Fire_Size,
+        Fire.Discovery_DOY,
+        Fire.Fire_year,
+        Fire.Fire_name
+    ]
+
+    big_results = db.session.query(*big).filter(Fire.Fire_year == year).all()
+
+    df_big = pd.DataFrame(big_results)
+
+    df_big = df_big.dropna()
+
+    df_big_sort = df_big.sort_values(by="Fire_Size", ascending = False).head(10)
+
+    df_big_sort = df_big_sort.reset_index()
+
+    df_big_dict = df_big_sort.to_dict(orient="record")
+
+    return jsonify(df_big_dict)
 
 
 if __name__ == "__main__": 
-    app.run(debug=True)
+    app.run()
--- static/js/app.js
+++ static/js/app.js
@@ -1,208 +1,208 @@
 function buildMetadata(year) {
 
-    console.log("I am inside build metadata");
-    // @TODO: Complete the following function that builds the metadata panel
-  
-    d3.json(`/metadata/${year}`).then(function(d) {
-      // this would return a json
-      // console.log(d)
-      // this would return an array of 7 lists 
-      // console.log(Object.entries(d));
-  
-      var PANEL = d3.select("#sample-metadata");
-  
-      PANEL.html("");
-  
-      // console.log(mData);
-      // console.log('I am in between mData and Panel')
-      // console.log(PANEL)
-      // console.log;
-  
-      // console.log("this here immediately is the mData")
-      // console.log(mData)
-  
-      // + ", " + d["Cumulative Days of Buring"]; 
-  
-      PANEL.selectAll("h6").data(d).enter().append("h6")
-        .text(function(d) { return `
-          Acres Burned: ${d["Acres_Burned"]} 
-          Cumulative Days of Burning: ${d["Cumulative_Days_of_Burning"]}
-          Number of Fires: ${d["Number_of_Fires"]}
-          `}); 
-  
-    });
-  
-  
-  
-  
-      // Use `.html("") to clear any existing metadata
-  
-      // Use `Object.entries` to add each key and value pair to the panel
-    
-  
-      // BONUS: Build the Gauge Chart
-      // buildGauge(data.WFREQ);
-  }
-   
-  function buildCharts(year) {
-  
-    console.log("I am inside build charts");
-  
-  
-    d3.json(`/years/${year}`).then((d) => {
-  
-      console.log(d)
-  
-  
-      var causes = []
-      var avgfiresize = []
-    
-      d.forEach(function(onefire) {
-        causes.push(onefire["Cause_Descr"]);
-        avgfiresize.push(onefire["Fire_Size"])
-      })
-  
-  
-      console.log(causes)
-      console.log(avgfiresize)
-  
-      // var acres = d.Fire_Size
-      // var fire_d = d[2][2]
-  
-      // console.log("Printing data")
-      // console.log(acres)
-      // console.log(fire_d)
-      
-    
-    // "d" is the api return 
-    
-    var barTrace = {
-      x: causes,
-      y: avgfiresize,
-      type: "bar",
-      marker: {
-        color: 'rgb(228, 132, 5)'
-      }
-      };
-  
-    var barData = [barTrace]
-    
-    var barLayout = {
-      title: "Cause of Fires",
-      xaxis: {title: "Cause of Fire"},
-      yaxis: {title: "Average Acres Burned"}
-    };
-    Plotly.newPlot("bar", barData, barLayout)
+  console.log("I am inside build metadata");
+  // @TODO: Complete the following function that builds the metadata panel
+
+  d3.json(`/metadata/${year}`).then(function(d) {
+    // this would return a json
+    // console.log(d)
+    // this would return an array of 7 lists 
+    // console.log(Object.entries(d));
+
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+
+    // console.log(mData);
+    // console.log('I am in between mData and Panel')
+    // console.log(PANEL)
+    // console.log;
+
+    // console.log("this here immediately is the mData")
+    // console.log(mData)
+
+    // + ", " + d["Cumulative Days of Buring"]; 
+
+    PANEL.selectAll("h6").data(d).enter().append("h6")
+      .text(function(d) { return `
+        Acres Burned: ${d["Acres_Burned"]} 
+        Cumulative Days of Burning: ${d["Cumulative_Days_of_Burning"]}
+        Number of Fires: ${d["Number_of_Fires"]}
+        `}); 
+
   });
+
+
+
+
+    // Use `.html("") to clear any existing metadata
+
+    // Use `Object.entries` to add each key and value pair to the panel
   
+
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+}
+ 
+function buildCharts(year) {
+
+  console.log("I am inside build charts");
+
+
+  d3.json(`/years/${year}`).then((d) => {
+
+    console.log(d)
+
+
+    var causes = []
+    var avgfiresize = []
   
-    d3.json(`/bigfires/${year}`).then((d) => {
-  
-      console.log(d)
+    d.forEach(function(onefire) {
+      causes.push(onefire["Cause_Descr"]);
+      avgfiresize.push(onefire["Fire_Size"])
+    })
+
+
+    console.log(causes)
+    console.log(avgfiresize)
+
+    // var acres = d.Fire_Size
+    // var fire_d = d[2][2]
+
+    // console.log("Printing data")
+    // console.log(acres)
+    // console.log(fire_d)
     
-      var doy = []
-      var topfires = []
-      var name = []
-      var desired_maximum_marker_size = 100;
-    
-      d.forEach(function(onetop) {
-        doy.push(onetop["Discovery_DOY"]);
-        topfires.push(onetop["Fire_Size"]);
-        name.push(onetop["Fire_name"])
-      })
-    
-    console.log(doy)
-    console.log(topfires)
   
-    var bubbleTrace = {
-      x: doy,
-      y: topfires,
-      mode: "markers",
-      text: name,
-      marker: {
-        size: topfires,
-        sizemode: 'area',
-        sizeref: 2.0 * Math.max(...topfires) / (desired_maximum_marker_size**2),
-        color: topfires,
-        colorscale: "'YIOrRd'"
-      }
-    }; 
-    
-    var bubbleData = [bubbleTrace]
+  // "d" is the api return 
   
-    var bubbleLayout = {
-      title: "Top Ten Largest Fires",
-      xaxis: {title: "Day of the Year"},
-      yaxis: {title: "Fire Size"}
+  var barTrace = {
+    x: causes,
+    y: avgfiresize,
+    type: "bar",
+    marker: {
+      color: 'rgb(228, 132, 5)'
+    }
     };
-    
-    
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout)
-    
+
+  var barData = [barTrace]
   
-    // var pieTrace = {
-    //   values: fire_d,
-    //   labels: fire_d,
-    //   marker: {
-    //     colorscale: "Earth"
-    //   },
-    //   type: "pie"
-    // }; 
+  var barLayout = {
+    title: "Cause of Fires",
+    xaxis: {title: "Cause of Fire"},
+    yaxis: {title: "Average Acres Burned"}
+  };
+  Plotly.newPlot("bar", barData, barLayout)
+});
+
+
+  d3.json(`/bigfires/${year}`).then((d) => {
+
+    console.log(d)
   
-    // var pieLayout = {
+    var doy = []
+    var topfires = []
+    var name = []
+    var desired_maximum_marker_size = 100;
   
-    // };
+    d.forEach(function(onetop) {
+      doy.push(onetop["Discovery_DOY"]);
+      topfires.push(onetop["Fire_Size"]);
+      name.push(onetop["Fire_name"])
+    })
   
-    // var pieData = [pieTrace] 
+  console.log(doy)
+  console.log(topfires)
+
+  var bubbleTrace = {
+    x: doy,
+    y: topfires,
+    mode: "markers",
+    text: name,
+    marker: {
+      size: topfires,
+      sizemode: 'area',
+      sizeref: 2.0 * Math.max(...topfires) / (desired_maximum_marker_size**2),
+      color: topfires,
+      colorscale: "'YIOrRd'"
+    }
+  }; 
   
-    // Plotly.newPlot("pie", pieData, pieLayout)
-  
-  })
-  
-      // @TODO: Build a Bubble Chart using the sample data
-  
-      // @TODO: Build a Pie Chart
-      // HINT: You will need to use slice() to grab the top 10 sample_values,
-      // otu_ids, and labels (10 each).
-  }
-  
-  function init() {
-  
-    console.log("I am inside init");
-  
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-  
-    // Use the list of sample names to populate the select options
-    d3.json("/years").then((fireYears) => {
-  
-  
-      fireYears.forEach((d) => {
-        
-        selector
-          .append("option")
-          .text(d)
-          .property("value", d);
-      });
-  
-      // Use the first sample from the list to build the initial plots
-      const firstYear = fireYears[0];
+  var bubbleData = [bubbleTrace]
+
+  var bubbleLayout = {
+    title: "Top Ten Largest Fires",
+    xaxis: {title: "Day of the Year"},
+    yaxis: {title: "Fire Size"}
+  };
   
   
-      buildMetadata(firstYear);
-      buildCharts(firstYear);
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout)
   
+
+  // var pieTrace = {
+  //   values: fire_d,
+  //   labels: fire_d,
+  //   marker: {
+  //     colorscale: "Earth"
+  //   },
+  //   type: "pie"
+  // }; 
+
+  // var pieLayout = {
+
+  // };
+
+  // var pieData = [pieTrace] 
+
+  // Plotly.newPlot("pie", pieData, pieLayout)
+
+})
+
+    // @TODO: Build a Bubble Chart using the sample data
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+}
+
+function init() {
+
+  console.log("I am inside init");
+
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/years").then((fireYears) => {
+
+
+    fireYears.forEach((d) => {
+      
+      selector
+        .append("option")
+        .text(d)
+        .property("value", d);
     });
-  }
-  
-  function optionChanged(newYear) {
-  
-    console.log("I am inside option changed");
-    console.log(newYear)
-    // Fetch new data each time a new sample is selected
-    buildCharts(newYear);
-    buildMetadata(newYear);
-  }
-  
-  // Initialize the dashboard
-  init();
+
+    // Use the first sample from the list to build the initial plots
+    const firstYear = fireYears[0];
+
+
+    buildMetadata(firstYear);
+    buildCharts(firstYear);
+
+  });
+}
+
+function optionChanged(newYear) {
+
+  console.log("I am inside option changed");
+  console.log(newYear)
+  // Fetch new data each time a new sample is selected
+  buildCharts(newYear);
+  buildMetadata(newYear);
+}
+
+// Initialize the dashboard
+init();
